a274544fc9ae47b2afd33ae488598e3cb066d7be
theravenstone <theravenstonepsn@gmail.com>
2021-03-10 22:13:00 +0100

    fixed error if player and dealer have same score Removed big "RAVEN" text on startup

--- blackjack.py
+++ blackjack.py
@@ -101,21 +101,14 @@ def score(dealer_hand, player_hand):
     elif total(player_hand) > total(dealer_hand):
         print_results(dealer_hand, player_hand)
         print("Congratulations. Your score is higher than the dealer. You win\n")
+    else:
+        print_results(dealer_hand, player_hand)
+        print("Tie! You and the dealer have the same score!\n")
 
 
 def game():
     choice = 0
     clear()
-    print('\n')
-    print('8888888b.         d8888 888     888 8888888888 888b    888')
-    print('888   Y88b       d88888 888     888 888        8888b   888')
-    print('888    888      d88P888 888     888 888        88888b  888')
-    print('888   d88P     d88P 888 Y88b   d88P 8888888    888Y88b 888')
-    print('8888888P"     d88P  888  Y88b d88P  888        888 Y88b888')
-    print('888 T88b     d88P   888   Y88o88P   888        888  Y88888')
-    print('888  T88b   d8888888888    Y888P    888        888   Y8888')
-    print('888   T88b d88P     888     Y8P     8888888888 888    Y888')
-    print('\n')
     print('Welcome to RAVEN Blackjack!')
     print('\n')
     dealer_hand = deal(deck)
